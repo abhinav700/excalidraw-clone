75d814432e67550f03fc69e0476435197878272f
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2025-03-15 00:12:12 +0530

    refactor

--- apps/http-backend/src/middlewares/gateRoom.ts
+++ apps/http-backend/src/middlewares/gateRoom.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from "../routers/userRouter";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const gateRoom = async (req:Request ,res:Response, next:NextFunction) => {
   try{
--- apps/http-backend/src/routers/userRouter.ts
+++ apps/http-backend/src/routers/userRouter.ts
@@ -3,29 +3,67 @@ import jwt from "jsonwebtoken";
 import {JWT_SECRET} from "@repo/backend-common/config";
 import {createUserSchema, SignUserSchema} from "@repo/common/types";
 import {prisma} from "@repo/db/client"
+import bcrypt from "bcryptjs"
 
 const userRouter: Router = Router();
 
 userRouter.post("/signup", async (req:Request, res: Response) =>{
   try{
   
-    const {username, password, name} = createUserSchema.parse(await req.body());
+    const {email, password, name} = createUserSchema.parse(await req.body());
+    
     const user = prisma.user.findFirst({
       where:{
         email
       }
     })
-    res.send("user created successfully");
+    
+    if(user != null)
+      res.status(400).json({error:"User already exists"})
+   console.log("Code Executing after sending response") 
+
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await prisma.user.create({
+      data:{
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+
+    res.status(200).json({user: newUser})
   } catch(err){
     console.log(err);
+    res.status(500).json({error: "Internal server error"});
   }
 })
 
 userRouter.post("/signin", async (req:Request, res:Response) => {
-  const {username, password} = SignUserSchema.parse(await req.body());
-  const token = jwt.sign(username, JWT_SECRET);
+  try{
+    const {email, password} = SignUserSchema.parse(await req.body());
+    const user = await prisma.user.findFirst({
+      where:{
+        email
+      }
+    })
 
-  res.send(token);
+    if(!user)
+    res.status(400).json({error: "User does not exist"});
+
+    const isCorrectPassword: boolean = await bcrypt.compare(password, user!.password)
+
+    if(!user)
+      res.status(400).json({error: "Incorrect password"});
+
+    const token = jwt.sign(email, JWT_SECRET);
+    res.status(200).json(token)
+  } catch(err){
+    console.log(err)
+    res.status(500).json({error:"Internal server error"});
+  }
 })
 
 userRouter.post("/room", (req:Request, res: Response) => {
